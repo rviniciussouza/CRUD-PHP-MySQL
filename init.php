639e10ece3bb358e46338526d6ee124088db6723
Vinicius Rodrigues <r.viniciussouza@outlook.com>
2019-03-01 02:20:31 -0300

    add init.php
    Arquivo de configuração para conexão do banco de dados

---
+++ init.php
@@ -0,0 +1,8 @@
+<?php
+//timezone
+date_default_timezone_set('America/Sao_Paulo');
+// conexão com o banco de dados
+define('BD_SERVIDOR','localhost');
+define('BD_USUARIO','root');
+define('BD_SENHA','12345');
+define('BD_BANCO','livraria');
